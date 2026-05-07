433213ef48f01f23aa5e283ba19d4d176d116721
burhanzafar227-svg <burhanzafar227@gmail.com>
2026-05-07 10:47:16 +0500

    commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -40,6 +40,7 @@ export default function Page() {
             <Contact />
             <InteractiveOrb />
             <Footer onNavigate={handleNavigate} />
+            
         </div>
     );
 }
